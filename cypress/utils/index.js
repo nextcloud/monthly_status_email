1d794f7126be5d9e94c2e27fc63b97e633dcc5da
Jonas <jonas@freesources.org>
2026-07-26 17:22:04 +0200

    test(cypress): fix session login
    Signed-off-by: Jonas <jonas@freesources.org>

--- cypress/utils/index.js
+++
@@ -1,9 +0,0 @@
-/**
- * SPDX-FileCopyrightText: 2023 Nextcloud GmbH and Nextcloud contributors
- * SPDX-License-Identifier: AGPL-3.0-or-later
- */
-
-import { User } from '@nextcloud/cypress' // eslint-disable-line
-
-export const randHash = () => Math.random().toString(36).replace(/[^a-z]+/g, '').slice(0, 10)
-export const randUser = () => new User(randHash())
