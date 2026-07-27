3c5b10b5cc410df2945488e6d7f7e99b85a7a392
Jonas <jonas@freesources.org>
2026-07-27 15:24:32 +0200

    chore(vue): use components from @nextcloud/vue
    Signed-off-by: Jonas <jonas@freesources.org>

--- src/components/PersonalSettings.vue
+++ src/components/PersonalSettings.vue
@@ -3,54 +3,29 @@
   - SPDX-License-Identifier: AGPL-3.0-or-later
 -->
 <template>
-	<div id="monthly-notifications-settings" class="section">
-		<h2>{{ t('monthly_status_email', 'Monthly Status Email') }}</h2>
-		<p class="settings-hint">
-			{{ t('monthly_status_email', 'Receive monthly status mails with a summary of usaged storage and usage hints') }}
-		</p>
-		<p>
-			<input
-				id="send-notifications"
-				v-model="sendNotifications"
-				type="checkbox"
-				class="checkbox">
-			<label for="send-notifications">{{ t('monthly_status_email', 'Send status email') }}</label>
-		</p>
-	</div>
+	<NcSettingsSection
+		:name="t('monthly_status_email', 'Monthly Status Email')"
+		:description="t('monthly_status_email', 'Receive monthly status mails with a summary of usaged storage and usage hints')">
+		<NcCheckboxRadioSwitch v-model="sendNotifications" type="switch">
+			{{ t('monthly_status_email', 'Send status email') }}
+		</NcCheckboxRadioSwitch>
+	</NcSettingsSection>
 </template>
 
-<script>
+<script setup>
 import axios from '@nextcloud/axios'
 import { loadState } from '@nextcloud/initial-state'
 import { t } from '@nextcloud/l10n'
 import { generateUrl } from '@nextcloud/router'
+import { ref, watch } from 'vue'
+import NcCheckboxRadioSwitch from '@nextcloud/vue/components/NcCheckboxRadioSwitch'
+import NcSettingsSection from '@nextcloud/vue/components/NcSettingsSection'
 
-export default {
-	name: 'PersonalSettings',
+const sendNotifications = ref(!loadState('monthly_status_email', 'opted-out', false))
 
-	setup() {
-		return { t }
-	},
-
-	data() {
-		return {
-			sendNotifications: !loadState('monthly_status_email', 'opted-out', false),
-		}
-	},
-
-	watch: {
-		sendNotifications() {
-			this.saveSetting()
-		},
-	},
-
-	methods: {
-		async saveSetting() {
-			const data = {
-				optedOut: !this.sendNotifications,
-			}
-			await axios.post(generateUrl('/apps/monthly_status_email/') + 'update', data)
-		},
-	},
-}
+watch(sendNotifications, async (value) => {
+	await axios.post(generateUrl('/apps/monthly_status_email/') + 'update', {
+		optedOut: !value,
+	})
+})
 </script>
